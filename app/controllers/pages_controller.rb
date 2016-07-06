b2711c5092c70a541a90fcce27920a100f959632
brendawg <brendanyong2013@gmail.com>
2016-07-06 10:45:53 +0000

    Intial commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,23 @@
+class PagesController < ApplicationController
+  def index
+  end
+  
+  def about
+  end
+  
+  def contact
+  end
+  
+  def apply
+  end
+  
+  def updates
+  end
+  
+  def graduates
+  end
+  
+  def promote
+  end
+  
+end
